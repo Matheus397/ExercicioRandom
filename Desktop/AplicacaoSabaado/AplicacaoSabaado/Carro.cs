3a8c4dfd19a45568ed16aa0cd499685fdeb3768d
matheus <matheus-397@bol.com.br>
2019-06-22 14:19:38 -0300

    carro1

---
+++ Desktop/AplicacaoSabaado/AplicacaoSabaado/Carro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AplicacaoSabaado
+{
+    public class Carro
+    {
+        public string Nome, Marca, PaisDeOrigem;
+        public DateTime DataDeFabricacao;       
+        public int QuantPortas, CapacidadeTanque, kmLitro;      
+           
+        public Carro(string nome, DateTime datafabricacao,int capacidade_tanque, int km_litro, int quant_portas, string marca, string pais_de_origem)
+        {
+            Nome = nome;
+            Marca = marca;
+            PaisDeOrigem = pais_de_origem;
+            DataDeFabricacao = datafabricacao;
+            CapacidadeTanque = capacidade_tanque;
+            kmLitro = km_litro;
+            QuantPortas = quant_portas;
+            FuncaoPrivada();
+
+        }
+
+        public int Velocidade() => 220;
+
+        public void FuncaoPrivada() { Nome = Nome.ToLower(); }
+
+        public double CalculoKilometragem (double km, double litro) { return km / litro; }
+    }
+}
---
+++ Desktop/AplicacaoSabaado/AplicacaoSabaado/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace AplicacaoSabaado
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flex;
+            char s;
+
+            Carro veiculo = new Carro("Golf", Convert.ToDateTime("18/06/2019"), 278, 8, 4, "Volkswagen", "Alemanha");
+
+            Console.WriteLine(DateTime.Now);
+
+            double km = 160.6; 
+            double litro = 74.5;
+
+            Console.WriteLine("O carro é flex? (s/n)");
+            s = Convert.ToChar(Console.ReadLine());
+
+            if (s == 's' || s == 'S')
+
+                flex = true;
+
+            else
+
+                flex = false;
+
+            Console.Write("O Cálculo da quilometragem por litro do veículo resulta em: ");
+            Console.WriteLine(veiculo.CalculoKilometragem(km, litro).ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.WriteLine(flex);
+
+            Console.WriteLine($"Modelo: {veiculo.Nome}\n" + $"Data Fabricação: {veiculo.DataDeFabricacao}\n" 
+           + $"Velocidade máxima: {veiculo.Velocidade()}\n" + $"Capacidade do tanque: {veiculo.CapacidadeTanque}\n" 
+           + $"Kilometragem po Litro {veiculo.kmLitro}\n" +  $"Quantidade de portas: {veiculo.QuantPortas}\n"  
+           + $"Marca: {veiculo.Marca}\n" + $"Pais de Origem: {veiculo.PaisDeOrigem}\n");
+
+        }
+    }
+}
